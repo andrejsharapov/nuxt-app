7dfb0d7c3035cfbc04ffcdde1ae9ed03c296cf45
Andrej Sharapov <an.sharapov90@ya.ru>
2021-02-24 14:24:04 +0300

    postcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,19 +9,25 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
-  css: [],
+  css: [
+    // { src: '~/assets/stylus/resets', lang: 'styl' },
+    { src: '~/assets/css/main' },
+  ],
+  styleResources: {
+    // sass: [],
+    scss: ['./assets/sass//utils/*.scss'],
+    // less: [],
+    // stylus: [],
+  },
   plugins: [],
   components: true,
   buildModules: [
     '@nuxtjs/eslint-module',
     '@nuxtjs/stylelint-module',
     '@nuxtjs/vuetify',
+    '@nuxtjs/style-resources',
   ],
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/pwa',
-    '@nuxt/content',
-  ],
+  modules: ['@nuxtjs/axios', '@nuxtjs/pwa', '@nuxt/content'],
   axios: {},
   pwa: {
     manifest: {
@@ -35,6 +41,25 @@ export default {
       dark: true,
     },
   },
-
-  build: {},
+  build: {
+    postcss: {
+      plugins: {
+        'postcss-custom-media': {},
+        'postcss-hexrgba': {},
+        'postcss-mixins': {},
+        'postcss-nested': {},
+        'postcss-responsive-type': {},
+        'postcss-sorting': {},
+        'postcss-url': {},
+        'postcss-utilities': {},
+        precss: {},
+      },
+      preset: {
+        features: {
+          'nesting-rules': true,
+          'custom-media-queries': true,
+        },
+      },
+    },
+  },
 }
