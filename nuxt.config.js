d53d2c68ba64b517302307469bf4beee8f6fd711
Andrej Sharapov <an.sharapov90@ya.ru>
2021-02-24 13:12:19 +0300

    initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  head: {
+    titleTemplate: '%s - nuxt-app',
+    title: 'nuxt-app',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+
+  css: [],
+
+  plugins: [],
+
+  components: true,
+
+  buildModules: [
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/stylelint-module',
+    '@nuxtjs/vuetify',
+  ],
+
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    '@nuxt/content',
+  ],
+
+  axios: {},
+
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
+
+  content: {},
+
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      dark: true,
+    },
+  },
+
+  build: {},
+}
