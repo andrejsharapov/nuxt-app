36ce873f3973f5f12745108b97c009ff051c2f74
Andrej Sharapov <an.sharapov90@ya.ru>
2021-03-02 00:44:49 +0300

    the component with MD language

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,9 @@
-<template>
-  <v-row justify="center" align="center" class="base">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="pa-16">
-        <h1>Hello</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa vel
-          quaerat hic repudiandae non exercitationem at
-          <a href="#" title=""> ipsum quibusdam placeat </a>
-          delectus, accusamus tempora laboriosam maiores consequuntur quisquam
-          pariatur eos aliquid. Voluptatem.
-        </p>
-      </v-card>
-    </v-col>
-  </v-row>
+<template lang="pug">
+v-container
+  v-row
+    v-col(cols='12')
+      v-card.pa-4
+        markdown 
 </template>
 
 <script>
