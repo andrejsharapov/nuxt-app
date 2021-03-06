893a127e92388c6fdf664c3e5843595c6aab25ae
Andrej Sharapov <an.sharapov90@ya.ru>
2021-03-06 20:15:25 +0300

    upd css box-shadow

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,12 @@ v-container
   v-row
     v-col(cols='12')
       v-card.pa-4
+        .d-flex.justify-space-around
+          v-sheet.ma-8.rounded.shadow-xs(width='100', height=100)
+          v-sheet.ma-8.rounded.shadow-sm(width='100', height=100)
+          v-sheet.ma-8.rounded.shadow-md(width='100', height=100)
+          v-sheet.ma-8.rounded.shadow-lg(width='100', height=100)
+          v-sheet.ma-8.rounded.shadow-xl(width='100', height=100)
         p Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur quasi necessitatibus veniam modi quas labore a ea, nihil inventore assumenda eius voluptatum maxime porro non sequi temporibus iure rerum velit.
 </template>
 
@@ -11,7 +17,7 @@ export default {
   layout: 'clean',
   head() {
     return {
-      titleTemplate: null,
+      titleTemplate: '',
       title:
         this.$t('author.name') +
         ' · ' +
