cda2207eb6cc4c7710a9c31dfd40fd718cf59b9a
Andrej Sharapov <an.sharapov90@ya.ru>
2021-03-08 18:15:46 +0300

    pages

---
+++ pages/articles/index.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.page__articles
+  //- history-back
+  v-container
+    v-row
+      v-col(cols='12')
+        v-card
+          v-card-title Hello!
+</template>
+
+<script>
+export default {
+  layout: 'clean',
+  data: () => ({
+    page: {
+      crumbs: '',
+      title: '',
+      description: '',
+    },
+  }),
+  head() {
+    return {
+      title: this.page.title,
+      titleTemplate: `%s · ${this.$t('author.name')}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.page.description,
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style>
+/* .page__articles {} */
+</style>
