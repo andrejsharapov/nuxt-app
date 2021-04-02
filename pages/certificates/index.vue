38288b78ed0b025221d84c8a724f828ddc18328a
Andrej Sharapov <an.sharapov90@ya.ru>
2021-04-03 01:41:11 +0300

    page/certificates

--- pages/certificates/index.vue
+++ pages/certificates/index.vue
@@ -1,23 +1,38 @@
 <template lang="pug">
-.page__
+.page__certificates
+  s-layout-components-back-image(:page='page')
+    template(#back-image)
+      s-back-flies-card-vertically
+
+    template(#list-items)
+      s-layout-components-back-image-list-grid(
+        :content='certItems',
+        :duration='2000'
+      )
+
   s-history-back
   v-container
     v-row
       v-col(cols='12')
-        v-card
-          v-card-title Hello!
+        pre {{ certItems }}
 </template>
 
 <script>
+import { certificates } from '~/lib/page-meta'
+
 export default {
-  layout: 'clean',
-  data: () => ({
-    page: {
-      crumbs: '',
-      title: '',
-      description: '',
-    },
-  }),
+  async asyncData({ $content, params }) {
+    const certItems = await $content('certificates', params.slug).fetch()
+
+    return {
+      certItems,
+    }
+  },
+  data() {
+    return {
+      page: certificates(this),
+    }
+  },
   head() {
     return {
       title: this.page.title,
@@ -35,5 +50,8 @@ export default {
 </script>
 
 <style>
-/* .page__ {} */
+.page__certificates {
+  --stop-color-one: #16c1e9;
+  --stop-color-two: #e3aefe;
+}
 </style>
