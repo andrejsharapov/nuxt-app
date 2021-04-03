c3fe5853c13b5487351d15bb37af8caf15c1da65
Andrej Sharapov <44722574+andrejsharapov@users.noreply.github.com>
2021-04-03 16:12:18 +0300

    Revert "page/certificates"

--- pages/certificates/index.vue
+++ pages/certificates/index.vue
@@ -1,49 +1,23 @@
 <template lang="pug">
-.page__certificates
+.page__
   s-history-back
-
-  s-layout-components-back-image(:page='page')
-    template(#back-image)
-      s-back-flies-card-vertically
-    template(#list-items)
-      s-layout-components-back-image-list-grid(
-        :content='certItems',
-        :duration='2000'
-      )
-
   v-container
     v-row
-      v-col(cols='12', sm='8', md='9') {{ $t("pages.certificates.message") }}
-      v-col.d-none.d-sm-block.mt-md-n16.text-center(
-        cols='12',
-        sm='4',
-        md='3',
-        style='z-index: 2'
-      )
-        s-fish-pages-certificates.mt-sm-n8.mt-md-n16(
-          width='250',
-          :height='null'
-        )
-
-      s-page-certificates-cert-items(:items='certItems')
+      v-col(cols='12')
+        v-card
+          v-card-title Hello!
 </template>
 
 <script>
-import { certificates } from '~/lib/page-meta'
-
 export default {
-  async asyncData({ $content, params }) {
-    const certItems = await $content('certificates', params.slug).fetch()
-
-    return {
-      certItems,
-    }
-  },
-  data() {
-    return {
-      page: certificates(this),
-    }
-  },
+  layout: 'clean',
+  data: () => ({
+    page: {
+      crumbs: '',
+      title: '',
+      description: '',
+    },
+  }),
   head() {
     return {
       title: this.page.title,
@@ -61,8 +35,5 @@ export default {
 </script>
 
 <style>
-.page__certificates {
-  --stop-color-one: #16c1e9;
-  --stop-color-two: #e3aefe;
-}
+/* .page__ {} */
 </style>
