06c8bd7de0e89ad416f18fe8ecc54eabd2a13b74
Andrej Sharapov <an.sharapov90@ya.ru>
2021-04-14 15:12:10 +0300

    page/articles

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -1,23 +1,53 @@
 <template lang="pug">
 .page__articles
   s-history-back
+
+  s-layout-components-back-image(:page='page')
+    template(#back-image)
+      s-back-flies-bubbles
+    template(#list-items)
+      s-layout-components-back-image-list-items(
+        :content='localeItems',
+        :icon='mdiCodeJson',
+        :duration='2000',
+        :message='$t("pages.shown")'
+      )
+
   v-container
     v-row
+      v-col(cols='12', sm='8', md='9')
+
+      v-col.mt-md-n16(cols='12', sm='4', md='3', style='z-index: 2')
+        s-fish-pages-articles.mt-sm-n8.mt-md-n16(width='260', :height='null')
+
       v-col(cols='12')
-        v-card
-          v-card-title Hello!
+        pre {{ localeItems }}
 </template>
 
 <script>
+import { mdiCodeJson } from '@mdi/js'
+import { articles } from '~/lib/page-meta'
+
 export default {
-  layout: 'clean',
-  data: () => ({
-    page: {
-      crumbs: '',
-      title: '',
-      description: '',
-    },
-  }),
+  async asyncData({ $content, params, app }) {
+    const articlesLocale = await $content(
+      `${app.i18n.locale}/articles`,
+      params.slug
+    )
+      .only('body')
+      .sortBy('updatedAt', 'desc')
+      .fetch()
+
+    return {
+      articlesLocale,
+    }
+  },
+  data() {
+    return {
+      page: articles(this),
+      mdiCodeJson,
+    }
+  },
   head() {
     return {
       title: this.page.title,
@@ -31,9 +61,10 @@ export default {
       ],
     }
   },
+  computed: {
+    localeItems() {
+      return this.articlesLocale ? this.articlesLocale : []
+    },
+  },
 }
 </script>
-
-<style>
-/* .page__articles {} */
-</style>
