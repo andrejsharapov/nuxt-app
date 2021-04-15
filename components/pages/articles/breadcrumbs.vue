678148fca0e1ee25cf33ea553c8cf30d19a8ea3c
Andrej Sharapov <an.sharapov90@ya.ru>
2021-04-16 00:29:40 +0300

    breadcrumbs

---
+++ components/pages/articles/breadcrumbs.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card class="mb-4 px-4 shadow-sm">
+    <v-breadcrumbs :items="items" divider="·" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ArtBreadcrumbs',
+  props: {
+    article: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data() {
+    return {
+      items: [
+        {
+          text: this.$t('pages.home.title'),
+          disabled: false,
+          href: this.localePath('/'),
+        },
+        {
+          text: this.$t('pages.articles.title'),
+          disabled: false,
+          href: this.localePath('/articles'),
+        },
+        {
+          text: this.article.title,
+          disabled: true,
+          href: '',
+        },
+      ],
+    }
+  },
+}
+</script>
