4fe35a7666ceabcc9117ba328b70a01242e2bb1a
Andrej Sharapov <an.sharapov90@ya.ru>
2021-05-01 00:28:23 +0300

    upd `JOB` component

--- components/layouts/job-offer.vue
+++ components/layouts/job-offer.vue
@@ -9,30 +9,30 @@
     offset-y='20',
     :color='$t("author.job.status") === "1" ? "success" : "grey darken-2"'
   )
-    v-hover(v-slot='{ hover }')
-      v-avatar
-        v-tooltip(bottom)
-          template(#activator='{ on: email }')
-            v-btn(
-              icon,
-              :disabled='$t("author.job.status") !== "1"',
-              :title='$t("author.job.title")',
-              href='mailto:an.sharapov90@gmail.com?subject=Предлагаем работу',
-              rel='noopener noreferrer',
-              v-on='email'
-            )
-              v-icon {{ hover ? mdiEmailOutline : mdiAccountCircleOutline }}
-          span {{ $t("author.job.title") }}
+    v-avatar
+      v-tooltip(bottom)
+        template(#activator='{ on: email }')
+          v-btn(
+            icon,
+            :href='`mailto:an.sharapov90@gmail.com?subject=${$t("author.job.title")}`',
+            rel='noopener noreferrer',
+            v-on='email'
+          )
+            v-icon {{ $t("author.job.status") !== 1 ? mdiBriefcaseVariantOffOutline : mdiBriefcaseVariantOutline }}
+        span.text-no-wrap {{ $t("author.job.title") }}
 </template>
 
 <script>
-import { mdiAccountCircleOutline, mdiEmailOutline } from '@mdi/js'
+import {
+  mdiBriefcaseVariantOutline,
+  mdiBriefcaseVariantOffOutline,
+} from '@mdi/js'
 
 export default {
   name: 'JobOffer',
   data: () => ({
-    mdiAccountCircleOutline,
-    mdiEmailOutline,
+    mdiBriefcaseVariantOutline,
+    mdiBriefcaseVariantOffOutline,
   }),
 }
 </script>
