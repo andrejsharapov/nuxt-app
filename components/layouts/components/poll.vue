8dd18297d5634bae5e412de0c320107c7727441f
Andrej Sharapov <an.sharapov90@ya.ru>
2022-03-05 11:47:40 +0300

    upd design

--- components/layouts/components/poll.vue
+++ components/layouts/components/poll.vue
@@ -2,22 +2,56 @@
 v-card.poll(color='purple')
   v-card-title.justify-center.white--text {{ $t("poll.title") }}
   v-card-subtitle.purple--text.text--lighten-4 {{ $t("poll.message") }}
-  v-dialog(v-if='!miniVariant', v-model='dialog', width='500')
+  v-dialog(
+    v-if='!miniVariant',
+    v-model='dialog',
+    width='500',
+    content-class='poll__modal'
+  )
     template(#activator='{ on: poll }')
       v-btn(block, text, dark, v-on='poll') {{ $t("poll.btn") }}
 
-    v-card.rounded-lg(tile, height='90vh')
-      iframe(
-        :src='"https://docs.google.com/forms/d/e/" + isLangVersion + "/viewform?embedded=true"',
-        width='100%',
-        height='100%',
-        frameborder='0',
-        marginheight='0',
-        marginwidth='0'
-      )
+    v-expansion-panels(v-model='panel', accordion, dark, multiple)
+      v-expansion-panel(:readonly='readonly')
+        v-expansion-panel-header.py-0.pr-2.pl-0(color='primary')
+          template(#default)
+            v-toolbar.pa-0(flat, color='transparent')
+              v-toolbar-title
+                v-icon(left) {{ mdiPoll }}
+                | {{ $t("poll.title") }}
+              v-spacer
+              v-btn(
+                icon,
+                @click='dialog = false',
+                @mousemove='readonly = true',
+                @mouseleave='readonly = false'
+              )
+                v-icon {{ mdiClose }}
+
+          template(#actions)
+            v-btn(
+              icon,
+              width='48',
+              height='48',
+              :class='{ "d-none": $vuetify.breakpoint.xs }'
+            )
+              v-icon {{ mdiChevronDown }}
+
+        v-expansion-panel-content.pa-0
+          v-card(flat, tile, height='75vh', color='white')
+            iframe(
+              :src='"https://docs.google.com/forms/d/e/" + isLangVersion + "/viewform?embedded=true"',
+              width='100%',
+              height='100%',
+              frameborder='0',
+              marginheight='0',
+              marginwidth='0'
+            )
 </template>
 
 <script>
+import { mdiClose, mdiChevronDown, mdiPoll } from '@mdi/js'
+
 export default {
   name: 'PollUsers',
   props: {
@@ -28,6 +62,11 @@ export default {
   },
   data: () => ({
     dialog: false,
+    readonly: false,
+    panel: [0],
+    mdiClose,
+    mdiChevronDown,
+    mdiPoll,
   }),
   computed: {
     isLangVersion() {
@@ -41,3 +80,9 @@ export default {
   },
 }
 </script>
+
+<style>
+.poll__modal .v-expansion-panel-content__wrap {
+  padding: 0;
+}
+</style>
