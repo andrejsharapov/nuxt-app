e0d285bd9ef5ec583d8aff946ba49669e858bb9b
Andrej Sharapov <an.sharapov90@ya.ru>
2022-03-21 21:19:53 +0300

    email design

---
+++ pages/cases/design/email/_slug.vue
@@ -0,0 +1,135 @@
+<template lang="pug">
+v-container.page__email__slug
+  s-history-back.mb-4
+  v-card-title {{ email.title }}
+  v-card-subtitle {{ formatDate(email.created) }}
+  v-card-text {{ $t("works.types") }}: {{ email.ux.price }}
+  v-row.mb-4
+    //- COMPONENT ARROW PREV
+    v-col.order-1.order-md-0.d-none.d-sm-flex.justify-start(cols='12', md='1')
+      s-pages-cases-components-slug-arrow-prev(
+        :prev='prev',
+        go-to='cases/design/email'
+      )
+    //- /COMPONENT
+
+    //- SECTION image
+    v-col(cols='12', md='5')
+      v-expansion-panels.mt-6.rounded(
+        v-model='panel',
+        flat,
+        :multiple='$vuetify.breakpoint.mdAndUp'
+      )
+        v-expansion-panel.shadow-sm.pa-0
+          v-expansion-panel-header(v-if='$vuetify.breakpoint.smAndDown') {{ $tc("events.show", 1) }} | {{ $tc("events.hide", 1) }}
+          v-expansion-panel-content.pa-0
+            v-img.mx-auto(
+              :alt='email.img.alt',
+              max-width='600',
+              lazy-src='https://fakeimg.pl/300/e9ecf2/1e1e24?text=IMG',
+              :src='email.img.src'
+            )
+              template(#placeholder)
+                v-row.fill-height.ma-0(align='center', justify='center')
+                  v-progress-circular(indeterminate, color='grey lighten-5')
+    //- /SECTION
+
+    //- SECTION info
+    v-col(cols='12', md='5')
+      v-timeline(dense)
+        v-timeline-item(
+          v-for='(s, i) in email.section',
+          :key='i',
+          small,
+          :color='colors[i]'
+        )
+          span(slot='opposite')
+          v-card.shadow-sm
+            v-card-title.text-subtitle-1.text-no-wrap.text-lg-h5 {{ s.title }}
+            v-divider(inset)
+            v-card-text(v-if='Array.isArray(s.caption)')
+              ol
+                li(v-for='(value, index) in s.caption', :key='index') {{ value }}
+            v-card-text(v-else) {{ s.caption }}
+    //- /SECTION
+
+    //- COMPONENT ARROW NEXT
+    v-col.order-1.order-md-0.d-none.d-sm-flex.justify-end(cols='12', md='1')
+      s-pages-cases-components-slug-arrow-next(
+        :next='next',
+        go-to='cases/design/email'
+      )
+    //- /COMPONENT
+</template>
+
+<script>
+export default {
+  name: 'CasesDesignEmailSlug',
+  middleware({ redirect, app }) {
+    if (app.i18n.locale === 'en') {
+      return redirect('301', `/${app.i18n.locale}/cases`)
+    }
+  },
+  async asyncData({ $content, app, params }) {
+    const email = await $content(
+      `${app.i18n.locale}/cases/design/email`,
+      params.slug
+    )
+      .where({ type: 'des-email' })
+      .fetch()
+
+    const [prev, next] = await $content(`${app.i18n.locale}/cases/design/email`)
+      .where({ type: 'des-email' })
+      .only(['title', 'slug'])
+      .sortBy('created', 'desc')
+      .surround(params.slug)
+      .fetch()
+
+    return {
+      email,
+      prev,
+      next,
+    }
+  },
+  data: () => ({
+    panel: [0],
+    colors: ['red', 'red', 'green', 'green', 'blue', 'blue'],
+  }),
+  head() {
+    return {
+      title: this.email?.title ? this.email.title : '',
+      titleTemplate: `%s · ${this.$t('pages.cases.title')} · ${this.$t(
+        'author.name'
+      )}`,
+    }
+  },
+  methods: {
+    formatDate(date) {
+      const options = { year: 'numeric', month: 'long', day: 'numeric' }
+      return new Date(date).toLocaleDateString(`${this.$i18n.locale}`, options)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.page__email__slug {
+  .case-arrow-link {
+    &__prev,
+    &__next {
+      position: fixed;
+      z-index: 1;
+      top: 49%;
+      opacity: 0.4;
+
+      &:hover {
+        opacity: 1;
+      }
+    }
+  }
+
+  .v-expansion-panel-content__wrap {
+    padding: 0;
+  }
+}
+</style>
---
+++ pages/cases/design/email/index.vue
@@ -0,0 +1,163 @@
+<template lang="pug">
+.page__email
+  s-history-back
+
+  s-layout-components-back-image(:page='page')
+    template(#back-image)
+      s-back-waves-line(
+        absolute,
+        right='0',
+        bottom='-7',
+        left='0',
+        :width='null',
+        :height='null'
+      )
+    template(#list-items)
+      s-layout-components-back-image-list-items(
+        :content='localeItems',
+        :icon='mdiEmailFastOutline',
+        :duration='2000',
+        :message='$t("pages.shown")'
+      )
+
+  v-container
+    v-row
+      v-col(cols='12', sm='7')
+        p {{ $tc("pages.des-email.message") }}
+
+      v-col.d-none.d-sm-block.mt-sm-n15.text-center(
+        cols='12',
+        sm='5',
+        style='z-index: 2'
+      )
+        .mt-sm-n12
+          s-fish-pages-des-email(height='200', :width='null')
+
+      //- SECTION AIDA
+      v-col(cols='12')
+        s-section-heading-anchor(
+          :title='$t("pages.des-email.marketing.title")',
+          anchor='marketing'
+        )
+        p {{ $t("pages.des-email.marketing.message") }}
+        p {{ $t("pages.des-email.aida.about") }}
+
+        v-list.aida(dense, color='transparent')
+          v-list-item(
+            v-for='i of $t("pages.des-email.aida.decoding")',
+            :key='i'
+          )
+            v-list-item-title {{ i }}
+      //- /SECTION
+
+      //- SECTION WORKS
+      v-col(cols='12')
+        s-section-heading-anchor(:title='$t("works.examples")', anchor='works')
+
+        .d-flex(v-if='localeItems.length')
+          v-spacer
+          v-btn-toggle(
+            v-if='$vuetify.breakpoint.mdAndUp',
+            v-model='cardView',
+            mandatory,
+            borderless,
+            color='primary'
+          )
+            v-btn(:ripple='false')
+              v-icon {{ mdiViewGrid }}
+            v-btn(v-if='$vuetify.breakpoint.mdAndUp', :ripple='false')
+              v-icon {{ mdiViewDay }}
+
+        v-row(v-if='localeItems.length')
+          v-col(
+            v-for='(item, index) in localeItems',
+            :key='index',
+            cols='12',
+            :md='cardView === 0 ? "4" : "12"'
+          )
+            s-pages-cases-components-card-grid(
+              v-if='cardView === 0',
+              position='top center',
+              :site='item'
+            )
+            s-pages-cases-components-card-row(
+              v-if='cardView === 1',
+              position='top center',
+              :site='item'
+            )
+        lazy-s-works-not-found(v-else, :message='$t("works.works-not-found")')
+      //- /SECTION
+</template>
+
+<script>
+import { mdiEmailFastOutline, mdiViewGrid, mdiViewDay } from '@mdi/js'
+import { desEmail as email } from '~/lib/page-meta'
+
+export default {
+  name: 'CasesDesignEmailIndex',
+  // REVIEW remove after filling en_US
+  // middleware({ redirect, app }) {
+  //   if (app.i18n.locale === 'en') {
+  //     return redirect('301', `/${app.i18n.locale}/cases`)
+  //   }
+  // },
+  async asyncData({ $content, app, params }) {
+    const emailLocale = await $content(
+      `${app.i18n.locale}/cases/design/email`,
+      params.slug
+    )
+      .sortBy('date', 'desc')
+      .fetch()
+
+    return {
+      emailLocale,
+    }
+  },
+  data() {
+    return {
+      page: email(this),
+      mdiEmailFastOutline,
+      mdiViewGrid,
+      mdiViewDay,
+      cardView: 0,
+    }
+  },
+  head() {
+    return {
+      title: this.page.title,
+      titleTemplate: `%s · ${this.$t('author.name')}`,
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: this.page.description,
+        },
+      ],
+    }
+  },
+  computed: {
+    localeItems() {
+      return this.emailLocale?.length ? this.emailLocale : []
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+.page {
+  &__email {
+    --stop-color-one: #d81943;
+    --stop-color-two: #810a99;
+
+    .aida {
+      .v-list-item {
+        &:last-child {
+          &:not(:hover) {
+            opacity: 0.15;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
